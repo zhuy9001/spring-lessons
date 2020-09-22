227b652bbd6426d45b087e2b2b7b0931af7299c9
zhuyang <zhuyang@pgl-world.com>
2020-09-22 17:19:39 +0800

    提交BeanDefinitionDemo

--- spring-bean/src/main/java/com/zhuyc/spring/bean/definition/AnnotationBeanDefinitionDemo.java
+++ spring-bean/src/main/java/com/zhuyc/spring/bean/definition/AnnotationBeanDefinitionDemo.java
@@ -1,9 +1,13 @@
 package com.zhuyc.spring.bean.definition;
 
 import com.zhuyc.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.Map;
 
@@ -19,6 +23,13 @@ public class AnnotationBeanDefinitionDemo {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext applicationContext=new AnnotationConfigApplicationContext();
 		applicationContext.register(Conifg.class);
+
+		// 通过 BeanDefinition 注册API 实现
+		// 1. 命名 Bean的 注册方式
+		registerUserBeanDefinition(applicationContext,"zhu-user");
+		//2. 非命名 Bean的 注册方式
+		registerUserBeanDefinition(applicationContext);
+
 		applicationContext.refresh();
 //		Map<String,Conifg> configBeans=applicationContext.getBeansOfType(Conifg.class);
 
@@ -34,6 +45,28 @@ public class AnnotationBeanDefinitionDemo {
 		applicationContext.close();
 	}
 
+	/**
+	 *  命名 Bean的 注册方式
+	 * @param registry
+	 * @param beanName
+	 */
+	public static void registerUserBeanDefinition(BeanDefinitionRegistry registry, String beanName){
+		BeanDefinitionBuilder beanDefinitionBuilder=BeanDefinitionBuilder.genericBeanDefinition(User.class);
+		beanDefinitionBuilder.addPropertyValue("id", 1L).addPropertyValue("name", "小哥");
+
+		//判断 beanName 是否为空，如果为空
+		if(StringUtils.hasText(beanName)){
+			//注册 BeanDefinition
+			registry.registerBeanDefinition(beanName, beanDefinitionBuilder.getBeanDefinition());
+		}else{
+			//非命名的注册方法
+			BeanDefinitionReaderUtils.registerWithGeneratedName(beanDefinitionBuilder.getBeanDefinition(),registry);
+		}
+	}
+
+	public static void registerUserBeanDefinition(BeanDefinitionRegistry registry){
+		registerUserBeanDefinition(registry,null);
+	}
 
 	// 通过  @Component 方式
 	@Component
